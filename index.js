54e778915117c2b13063955271ecffae517f4bf1
Botium <florian.treml@botium.at>
2020-03-20 16:47:27 +0000

    Botium Core Branching sample

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-const Mocha = require('mocha')
+const { BotDriver } = require('botium-core')
 
-const mocha = new Mocha()
-mocha.addFile('./spec/botium.spec.js')
+const turn = async (container, text) => {
+  await container.UserSaysText(text)
+  console.log("SENT: " + text)
+  const answer = await container.WaitBotSaysText()
+  console.log("ANSWER: " + answer)
+  return answer
+}
 
-console.log('running')
-mocha.run(failures => process.exitCode = failures ? 1 : 0)
-console.log('ready')
+(async () => {
+  try {
+    const driver = new BotDriver()
+    const container = await driver.Build()
+    await container.Start()
+
+    let answer = await turn(container, "order number 1")
+    if (answer.startsWith("here are the details for order")) {
+      await turn(container, "thank you")
+    } else {
+      await turn(container, "thanks for nothing")
+    }
+  } catch (err) {
+    console.log('FAILED ', err)
+  }
+})()
