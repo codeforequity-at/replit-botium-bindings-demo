1ba5d72692a22b6f7829479502eef59de5178aa4
Botium <florian.treml@botium.at>
2022-02-23 11:28:06 +0000

    Cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-const Mocha = require('mocha')
-
-const mocha = new Mocha()
-mocha.addFile('./spec/botium.spec.js')
-
-console.log('running')
-mocha.run(failures => process.exitCode = failures ? 1 : 0)
-console.log('ready')
